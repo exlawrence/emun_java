147aba9e710367c0e26d19d195d7bad0878d9617
exlawrence <digo_kbdlo@hotmail.com>
2022-07-25 04:59:21 -0300

    Alterações e comentários

--- src/application/Program.java
+++ src/application/Program.java
@@ -9,9 +9,18 @@ public class Program {
 
 	public static void main(String[] args) {
 
-		Order order = new Order(1025, new Date(), OrderStatus.DEVILERED);
+		Order order = new Order(1025, new Date(), OrderStatus.DELIVERED);
 		
 		System.out.println(order);
+		
+		
+		//obs: para instanciar um tipo do Order(enum), não é preciso usar o new!
+		OrderStatus os1 = OrderStatus.DELIVERED;
+		System.out.println(os1);
+		//Conversão de String para enum
+		OrderStatus os2 = OrderStatus.valueOf("DELIVERED");
+		System.out.println(os2);
+		
 		
 	}
 

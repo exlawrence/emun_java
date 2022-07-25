eca75c5595479e5afe56ecc380246d8e7d342b67
exlawrence <digo_kbdlo@hotmail.com>
2022-07-25 05:02:21 -0300

    finalizado

--- src/application/Program.java
+++ src/application/Program.java
@@ -14,7 +14,6 @@ public class Program {
 		System.out.println(order);
 		
 		
-		//obs: para instanciar um tipo do Order(enum), não é preciso usar o new!
 		OrderStatus os1 = OrderStatus.DELIVERED;
 		System.out.println(os1);
 		//Conversão de String para enum
